88c1f0baacf1ce47ca196299e6076ce7803ad1cc
verickmr <verickmr.dev@gmail.com>
2025-11-20 21:55:03 -0300

    feat: mongoclient with mongo db conection

---
+++ src/database/mongo.ts
@@ -0,0 +1,20 @@
+import { log } from "console";
+import { MongoClient as MC, Db} from "mongodb";
+export const MongoClient = {
+    client: undefined as unknown as MC,
+    db:undefined as unknown as Db,
+
+    async connect(): Promise<Void>{
+        const url = process.env.MONGODB_URL || "localhost:27017";
+        const username = process.env.MONGODB_USERNAME;
+        const password = process.env.MONGODB_PASSWORD;
+
+        const client = new MC(url,{auth:{username,password}})
+        const db = client.db("emterprise")
+
+        this.client =client;
+        this.db = db
+        log("Connected to MongoDB");
+    }
+
+}
